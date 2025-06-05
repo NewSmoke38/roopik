7de0258939a65133e063cfa83bd187c97bcae6b3
NewSmoke38 <shivanialive2007@gmail.com>
2025-06-06 04:46:58 +0530

    Add dependencies for CORS and Express in package.json, created app.js

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import generateCard from './logic/cardGen.js'; 
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+app.use(express.json());
+
+app.post('/generate', (req, res) => {
+  const { fullname, username, roles } = req.body;
+  if (!fullname || !username || !roles || roles.length < 1) {
+    return res.status(400).json({ error: 'Missing fields' });
+  }
+
+  const card = generateCard(fullname, username, roles);
+  res.json(card);
+});
+
+const PORT = process.env.PORT || 3000;    /// app starts listening
+app.listen(PORT, () => {
+  console.log(`Roopik backend running at http://localhost:${PORT}`);
+});
