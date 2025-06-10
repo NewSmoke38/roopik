24d2afc8ccade2928ddfc310958959824fa7e51d
Shivani <shivanialive2007@gmail.com>
2025-06-11 03:16:08 +0530

    fix: remove missing App.css import to resolve Vite build error

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CardPage from './pages/CardPage';
-import './App.css';
+import './assets/App.css';
 
 function App() {
   return (
